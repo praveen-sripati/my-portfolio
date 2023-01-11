49082a5bc2fae35c2b5a50856e3449875646ec15
Praveen Sripati <pvnsripati@gmail.com>
2023-01-12 00:10:38 +0530

    Updates

---
+++ internal/immutable/chunks/0-85ea407e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-d71cc220.js";export{t as component};
---
+++ internal/immutable/chunks/2-bc039e71.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-f0ea090e.js";export{t as component};
